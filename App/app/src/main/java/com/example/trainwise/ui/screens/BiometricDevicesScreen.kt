cbefa5b9c9747edef6a9b59a865476acb41a0844
DiegoA4476 <diegoaguilar@ua.pt>
2026-05-07 09:52:47 +0100

    unused imports

--- App/app/src/main/java/com/example/trainwise/ui/screens/BiometricDevicesScreen.kt
+++ App/app/src/main/java/com/example/trainwise/ui/screens/BiometricDevicesScreen.kt
@@ -3,7 +3,6 @@ package com.example.trainwise.ui.screens
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
--- App/app/src/main/java/com/example/trainwise/ui/screens/TrainingHistoryScreen.kt
+++ App/app/src/main/java/com/example/trainwise/ui/screens/TrainingHistoryScreen.kt
@@ -1,6 +1,5 @@
 package com.example.trainwise.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
